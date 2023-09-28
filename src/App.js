d328e09fe3ec081583939581544f9419cd49faa1
Shristi Gupta <shristiguptawork@gmail.com>
2023-09-28 20:49:42 +0530

    Add: added navbar and home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+     <Navbar/>
+     <Home />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 export default function Navbar(){
     return(
-        <div>
-            
+        <div className=" flex justify-end w-full">
+            <div className=" relative fixed shadow w-3/4 right-0 py-8 px-9 mt-9 mx-9 text-lg">
+                <ul className="flex gap-3 justify-center text-slate-500 font-[400]">
+                    <li className="cursor-pointer hover:text-slate-900">home</li>
+                    <li className="cursor-pointer hover:text-slate-900">about</li>
+                    <li className="cursor-pointer hover:text-slate-900">skills</li>
+                    <li className="cursor-pointer hover:text-slate-900">projects</li>
+                    <li className="cursor-pointer hover:text-slate-900">contacts</li>
+                </ul>
+            </div>
         </div>
     )
 }
